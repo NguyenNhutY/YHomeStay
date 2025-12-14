e753badcd2180d9c93e19cc3388bb5ec6c9c5272
NguyenNhutY <130396826+NguyenNhutY@users.noreply.github.com>
2025-12-14 15:12:28 +0700

    fix clerkUserController

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,59 +1,53 @@
 import { Webhook } from "svix";
-import User from "svix";
+import User from "../models/User.js";
 
 const clerkWebhooks = async (req, res) => {
   try {
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    const header = {
+    const payload = req.body; // raw buffer
+    const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     };
-    await whook.verify(JSON.stringify(req.body), headers);
 
-    const { data, type } = req.bobdy;
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    const evt = wh.verify(payload, headers);
+    const { data, type } = evt;
+
     switch (type) {
       case "user.created": {
-        const userData = {
+        await User.create({
           _id: data.id,
-          email: data.email_addresses[0].email_addresses,
-          username: data.first_name + "" + data.last_name,
+          email: data.email_addresses[0].email_address,
+          username: `${data.first_name || ""} ${data.last_name || ""}`,
           image: data.image_url,
-        };
-        await User.create(userData);
+        });
         break;
       }
 
       case "user.updated": {
-        const userData = {
-          _id: data.id,
-          email: data.email_addresses[0].email_addresses,
-          username: data.first_name + "" + data.last_name,
+        await User.findByIdAndUpdate(data.id, {
+          email: data.email_addresses[0].email_address,
+          username: `${data.first_name || ""} ${data.last_name || ""}`,
           image: data.image_url,
-        };
-
-        await User.findByIdAndUpdate(userData);
+        });
         break;
       }
 
       case "user.deleted": {
-        const userData = {
-          _id: data.id,
-          email: data.email_addresses[0].email_addresses,
-          username: data.first_name + "" + data.last_name,
-          image: data.image_url,
-        };
-
-        await User.findByIdAndDelete(userData);
+        await User.findByIdAndDelete(data.id);
         break;
       }
+
       default:
         break;
     }
-    res.json({ success: true, message: "Webhook Recieved" });
-  } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error("Clerk webhook error:", err.message);
+    res.status(400).json({ success: false });
   }
 };
 
