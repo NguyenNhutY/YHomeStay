ebe24c9cd6ec8f5c3ea007a7e0edfc6f182f8de1
NguyenNhutY <130396826+NguyenNhutY@users.noreply.github.com>
2025-12-14 21:40:37 +0700

    .

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,13 @@
-// config/mongodb.js
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
   try {
-    await mongoose.connect(process.env.MONGO_URI); // không cần options nữa
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
+    isConnected = true;
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
